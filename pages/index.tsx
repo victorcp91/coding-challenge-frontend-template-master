0090454bd0ebf0097621dfaefb9bebd279817ad9
Victor Corrêa <victor.correapereira@gmail.com>
2021-07-19 14:20:47 -0300

    search student

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Box, TextInput } from "grommet";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import UserCard from "../components/user.card";
 import { fetchStudents, Student } from "../services/students";
 
@@ -7,28 +7,47 @@ type Props = {};
 
 const Main: React.FC<Props> = ({}) => {
 
+  const [loadingError, setLoadingError] = useState<boolean>(false);
+  const [studentQuery, setStudentQuery] = useState<string>('');
   const [students, setStudents] = useState<Student[]>([]);
 
   const onChangeHandler = (event: any) => {
-    // TODO
+    setStudentQuery(event.currentTarget.value);
   };
 
   useEffect(() => {
     fetchStudents().then(res => {
       setStudents(res);
+    }).catch(() => {
+      setLoadingError(true);
     });
   }, []);
+
+  const filteredStudents = useMemo(() => {
+    if(studentQuery){
+      const formattedQuery = studentQuery.toLowerCase();
+      return students.filter(s =>
+        s.first_name.toLowerCase().includes(formattedQuery) || 
+        s.last_name.toLowerCase().includes(formattedQuery))
+    } return students;
+  }, [students, studentQuery]);
+
+  const emptyStudentsMessage = useMemo(() => {
+    if(loadingError){
+      return (<p>Loading students error</p>)
+    } return (<p>Student not found</p>);
+  }, [students, filteredStudents]);
 
 
   return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
-      <TextInput placeholder="type here" value="" onChange={onChangeHandler} />
+      <TextInput placeholder="type here" value={studentQuery} onChange={onChangeHandler}/>
       <Box direction="row" wrap={true}>
-        {students.map((s) => (
-          <Box key={s.id} margin="10px">
-            <UserCard user={s} />
-          </Box>
-        ))}
+      {!!filteredStudents.length ? filteredStudents.map((s) => (
+        <Box key={s.id} margin="10px">
+          <UserCard user={s} />
+        </Box>
+      )): emptyStudentsMessage}
       </Box>
     </Box>
   );
