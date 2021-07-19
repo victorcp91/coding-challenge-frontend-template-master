f1613430bbcfa2869fded6fb6f381a5401f72157
Victor Corrêa <victor.correapereira@gmail.com>
2021-07-19 13:41:30 -0300

    students search
    Fetch data related to students;
    Display data using the existing components;

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,29 @@ import { fetchStudents, Student } from "../services/students";
 type Props = {};
 
 const Main: React.FC<Props> = ({}) => {
+
+  const [students, setStudents] = useState<Student[]>([]);
+
   const onChangeHandler = (event: any) => {
     // TODO
   };
+
+  useEffect(() => {
+    fetchStudents().then(res => {
+      setStudents(res);
+    });
+  }, []);
+
 
   return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
       <TextInput placeholder="type here" value="" onChange={onChangeHandler} />
       <Box direction="row" wrap={true}>
-        {/* {students.map((s) => (
-          <Box margin="10px">
+        {students.map((s) => (
+          <Box key={s.id} margin="10px">
             <UserCard user={s} />
           </Box>
-        ))} */}
+        ))}
       </Box>
     </Box>
   );
